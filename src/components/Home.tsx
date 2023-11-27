91a14047f420277fca7d1a3d01a173717340fcc2
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-11-27 06:12:56 +0600

    feat: :fire: loader

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -105,6 +105,16 @@ export const Home = (props: Props) => {
               ) : null
             })}
           </ul>
+
+          {toDo?.length === 0 && (
+            <div className='flex items-center justify-center w-full'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor"
+                className="animate-spin h-10 text-[red] w-10">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+              </svg>
+            </div>
+          )}
+        
         </div>
       </div>
     </div>
